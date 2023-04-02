56ae2dae2071b01a06860119fb444c6dd9b63a29
Martin Moses <np88nxeqkczdxog23zbj@gmail.com>
2023-04-02 14:22:19 +0200

    trochu blíž funnkčnímu žebříku

---
+++ Project Cellar/Assets/Scripts/LadderMovement/PlayerLadder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerLadder : MonoBehaviour
+{
+    public Vector3 teleportPosition = new Vector3(1, 2, 3); // The coordinates where you want to teleport the player to.
+    public Tilemap leftLadderTilemap; // The Tilemap that represents the left ladder.
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Teleporting player to: " + teleportPosition);
+        if (collision.CompareTag("Player") && leftLadderTilemap.HasTile(leftLadderTilemap.WorldToCell(transform.position))) // If the player character enters the left ladder zone...
+        {
+            transform.position = teleportPosition; // ...teleport the player to the desired position.
+        }
+    }
+}
