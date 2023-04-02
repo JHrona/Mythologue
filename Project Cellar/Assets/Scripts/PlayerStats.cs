d48450143e12b826a98a17cb6fafed9dbada5444
JHrona <jjj2005@seznam.cz>
2023-04-02 15:08:58 +0200

    aaa
    aaa

--- Project Cellar/Assets/Scripts/PlayerStats.cs
+++ Project Cellar/Assets/Scripts/PlayerStats.cs
@@ -1,12 +1,15 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public static PlayerStats playerStats;
 
     public GameObject player;
+    public Text healthText;
+    public Slider healthSlider;
 
     public float health;
     public float maxHealth;
@@ -26,20 +29,31 @@ public class PlayerStats : MonoBehaviour
 
     void Start()
     {
-        health = maxHealth;    
+        health = maxHealth;
+        SetHealthUI();
     }
+
 
     public void DealDamage(float damage)
     {
         health -= damage;
         CheckDeath();
+        SetHealthUI();
     }
     
     public void HealCharacter(float heal)
     {
         health += heal;
         CheckOverheal();
+        SetHealthUI();
     }
+
+    private void SetHealthUI()
+    {
+        healthSlider.value = CalculateHealthPercentage();
+        healthText.text = Mathf.Ceil(health).ToString() + " / " + Mathf.Ceil(maxHealth).ToString();
+    }
+
 
     private void CheckOverheal()
     {
@@ -56,4 +70,10 @@ public class PlayerStats : MonoBehaviour
         Destroy(player);
         }
     }
+
+    float CalculateHealthPercentage()
+    {
+        return health / maxHealth;
+    }
+
 }
