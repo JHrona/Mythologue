f74910aad328549ce7735c905872fcbcdb14213b
JHrona <jjj2005@seznam.cz>
2023-04-12 21:06:17 +0200

    ai

--- Mythologue/Assets/Scripts/Enemy.cs
+++ Mythologue/Assets/Scripts/Enemy.cs
@@ -12,11 +12,24 @@ public class Enemy : MonoBehaviour
     public Slider healthBarSlider;
 
     public GameObject lootDrop;
+    public GameObject player;
+        public GameObject projectile;
+    public float minDamage;
+    public float maxDamage;
+    public float projectileForce;
+    public float cooldown;
+        protected GameObject target;
+          private float lastShotTime;
 
     void Start()
     {
-        health = maxHealth;    
+        health = maxHealth; 
+        Target();
+
     }
+
+
+
 
     public void DealDamage(float damage)
     {
@@ -55,4 +68,75 @@ public class Enemy : MonoBehaviour
     {
         return (health / maxHealth);
     }
+
+    private void Update()
+    {
+
+        if(player != null)
+        {
+        EnemyAI();
+
+        }
+        if(health <= 0)
+        {
+            healthBar.SetActive(false);
+        }
+                if(player != null)
+    {
+                float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance <= 5f && Time.time > lastShotTime + cooldown)
+    
+        {
+        ShootPlayer();
+        }   
+    }
+    }
+    
+    // function to move the enemy towards the player
+    public void MoveTowardsPlayer()
+    {
+        if (player != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 1f * Time.deltaTime);
+            }
+    }
+    // create enemy ai to move towards the player when the player is within a certain radius of the enemy
+
+    
+    // create a function to not shoot at the player if the enemy is moving towards the player
+    public void EnemyAI()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance >= 5f)
+        {
+            MoveTowardsPlayer();
+        }
+
+    }
+
+  private void ShootPlayer()
+    {
+        
+        if (target != null)
+        {
+            GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
+            Vector2 myPos = transform.position;
+            Vector2 targetPos = target.transform.position;
+            Vector2 direction = (targetPos - myPos).normalized;
+            spell.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+            spell.GetComponent<TestEnemyProjectile>().damage = Random.Range(minDamage, maxDamage);
+            lastShotTime = Time.time; // Ulož poslední čas střelby
+        }
+    }
+        private void Target()
+    {
+        if(player != null)
+        {
+        target = FindObjectOfType<Movement>().gameObject;
+        }
+        return;
+
+    }
+
+
 }
--- Mythologue/Assets/Scripts/PlayerStats.cs
+++ Mythologue/Assets/Scripts/PlayerStats.cs
@@ -95,4 +95,7 @@ public class PlayerStats : MonoBehaviour
         }
         */
     }
+
+
+
 }
